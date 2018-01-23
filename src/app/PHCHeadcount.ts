59255f51b7d8f9f71b8507c127b6990f2c8f7b33
mankgamc <mankgamc@gmail.com>
2018-01-23 08:58:42 +0200

    ....

---
+++ src/app/PHCHeadcount.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by Comfort Mawkga on 2018/01/22.
+ */
+export class PHCHeadcount {
+  totalHeadcountdatavalue: number;
+  period: string;
+  dataElement: string;
+  orgUnit: string;
+  dataSet: string;
+  categoryOptionCombo:string;
+  attributeOptionCombo:string;
+  storedBy: string;
+  created:string;
+  lastUpdated:string;
+  comment:string;
+  followUp: boolean;
+  uid:string;
+}
--- src/app/form-population-estimate/form-population-estimate.component.ts
+++ src/app/form-population-estimate/form-population-estimate.component.ts
@@ -4,6 +4,7 @@ import {DataElementService} from '../providers/dataelement.service';
 import {DatasetService} from '../providers/dataset.service';
 
 import {dataValues} from "../dataValues";
+import {PHCHeadcount} from "../PHCHeadcount";
 
 
 @Component({
@@ -21,6 +22,12 @@ export class FormPopulationEstimateComponent {
   private datasets: any;
   private dateElements: any;
 
+  private phcHeadCount: any;
+  private PHCHeadcountColection: PHCHeadcount[];
+
+  private PHCHeadcountObject: PHCHeadcount;
+
+
 
 
   OrgUnitArea: string=""
@@ -33,6 +40,7 @@ export class FormPopulationEstimateComponent {
 
 
    populationEstimatesDatavalues: dataValues[];
+   MonthlyPHCHeadCounntTotalsDatavalues: any;
 
   populationdataLoaded: boolean;
 
@@ -45,16 +53,20 @@ export class FormPopulationEstimateComponent {
     this.orgUnitLevel5Orgs = [];
     this.dateElements = [];
 
+    this.phcHeadcountDataElements = [];
+
     this.CurrentpopulationEstimates = [];
     this.populationEstimatesDatavalues = [];
 
-
+    this.MonthlyPHCHeadCounntTotalsDatavalues = [];
+    this.PHCHeadcountColection =  [];
+    this.PHCHeadcountObject  = new PHCHeadcount();
 
   }
   ngOnInit() {
     console.log("Boki Boki");
     const populationdataelementUrl ='http://localhost:8085/dhis/api/dataElements.json?filter=dataElementGroups.id:in:[Wi2PeVJPnpI]&paging=false&fields=*&paging=false';
-   const phcHeadcountDataElementsUrl = 'http://localhost:8085/dhis/api/dataElements.json?filter=id:in:[B5g5X2k5Q8k,EnwfwzfrdQ7,IovfSP4TNjF,pqkbzu3K48G,C6y6Q2v7L0i]&paging=false';
+   const phcHeadcountDataElementsUrl = 'http://localhost:8085/dhis/api/dataElements.json?filter=id:in:[B5g5X2k5Q8k,EnwfwzfrdQ7,IovfSP4TNjF,pqkbzu3K48G]&paging=false';
 
     const dataSetsUrl = 'http://localhost:8085/dhis/api/dataSets.json?paging=false&fields=id, name&filter=id:eq:L2hwAPHJyTd&paging=false';
     //const orgUnitLevelUrl = '../../training/api/organisationUnitLevels.json';
@@ -95,8 +107,17 @@ export class FormPopulationEstimateComponent {
   }
   onOrgunitSelection($event){
     this.populationEstimatesDatavalues = [];
+    this.phcHeadcountDataElements = [];
 
 let  populationEstimatesValues = 'http://localhost:8085/dhis/api/dataValueSets.json?dataSet=L2hwAPHJyTd&period=2016&orgUnit='+ $event.target.value;
+//let MonthlyPHCHeadcount = 'http://localhost:8085/dhis/api/dataValueSets.json?dataSet=ctgkIpBrhzn&period=201712&orgUnit='+ $event.target.value;
+    let MonthlyPHCHeadcount = 'http://localhost:8085/dhis/api/dataValueSets.json?dataSet=Ux8RWIJyIk3&period=201712&orgUnit=yb0qGdJuNeR'
+    let phcHeadcountDataElementsUrl = 'http://localhost:8085/dhis/api/dataElements.json?filter=id:in:[B5g5X2k5Q8k,EnwfwzfrdQ7,IovfSP4TNjF,pqkbzu3K48G,C6y6Q2v7L0i]&paging=false';
+
+    //use the URL to get children in given the parent Id
+    let OrgunitHierachy  = 'http://localhost:8085/dhis/api/organisationUnits.json?filter=parent.id:eq:BCpKOPuBh8C&paging=false'
+
+
     console.log('Organisation name is '+  $event.target.value);
     this.dataElementService.getDataelementsService(populationEstimatesValues).then(result => {this.CurrentpopulationEstimates =  result.dataValues
       console.log(result);
@@ -107,7 +128,6 @@ let  populationEstimatesValues = 'http://localhost:8085/dhis/api/dataValueSets.j
         let populaestimateLine= new dataValues();
 
         let DataElementUrl = 'http://localhost:8085/dhis/api/dataElements.json?paging=false&filter=id:eq:'+pop.dataElement;
-
         this.dataElementService.getDataelementsService(DataElementUrl).then(result => {dataElementName =  result.dataElements[0].name
 
           populaestimateLine.dataElement = dataElementName;
@@ -118,27 +138,74 @@ let  populationEstimatesValues = 'http://localhost:8085/dhis/api/dataValueSets.j
           populaestimateLine.uid = pop.dataElement
           this.populationEstimatesDatavalues.push(populaestimateLine);
 
+
         }).catch(error => console.log(error));
       }
+      this.phcHeadcountDataElements = [];
+      //Get population Headcount
+      this.dataElementService.getDataelementsService(phcHeadcountDataElementsUrl).then(result => {
+        this.phcHeadcountDataElements =  result.dataElements
+        console.log(result)
+
+        this.PHCHeadcountColection = [];
+
+        for (let dataElements of  this.phcHeadcountDataElements)
+        {
+        //  console.log("ffffffffffffffffffffffffffffffffffffffffffffffffff"+dataElements.id)
+          let yearlyHeadCountTotal: number=0;
+          this.PHCHeadcountObject  = new PHCHeadcount();
+          this.PHCHeadcountObject.totalHeadcountdatavalue = 0;
+
+          this.dataElementService.getDataelementsService(MonthlyPHCHeadcount).then(result => { this.MonthlyPHCHeadCounntTotalsDatavalues =  result.dataValues
+            //  console.log(result)
+            for (let phcHeadCount of  this.MonthlyPHCHeadCounntTotalsDatavalues )
+            {
+              this.PHCHeadcountObject.dataElement = ""
+              this.PHCHeadcountObject.period = ""
+              this.PHCHeadcountObject.dataSet = ""
+              this.PHCHeadcountObject.orgUnit = ""
+              if (phcHeadCount.dataElement == dataElements.id){
+
+                console.log(phcHeadCount.value)
+                if (!isNaN( Number(phcHeadCount.value))){
+
+                  if (yearlyHeadCountTotal==0){
+                    yearlyHeadCountTotal= Number(phcHeadCount.value)
+
+                  }else
+                  {
+                    yearlyHeadCountTotal=yearlyHeadCountTotal+Number(phcHeadCount.value)           }
+
+                  this.PHCHeadcountObject.totalHeadcountdatavalue = yearlyHeadCountTotal;
+                  this.PHCHeadcountObject.dataElement = phcHeadCount.dataElement
+                  this.PHCHeadcountObject.period = phcHeadCount.period
+                  this.PHCHeadcountObject.dataSet = result.dataSet
+                  this.PHCHeadcountObject.orgUnit = phcHeadCount.orgUnit
+                  console.log("I am In")
+                  console.log("Total :"+this.PHCHeadcountObject.totalHeadcountdatavalue)
+                }
+              }
+            }
+            this.PHCHeadcountColection.push(this.PHCHeadcountObject)
+          }).catch(error => console.log(error));
+        }
+      }).catch(error => console.log(error));
+
+
     }).catch(error => console.log(error));
   }
-
-
-
   GetPopulationtotalstasaa(){
-
-
   }
 
-  GetTotalHeadCount(){
-
-  }
+  GetTotalHeadCount(){  }
 
 
   CalculatePopulationProportion(){}
 
 
   CalculatePouplationEstimates(){}
+
+
 
 
 
